809d4d37ba6e594047e60d8d53a7bfbe7d66110d
Anurag Rajawat <anurag@stepsecurity.io>
2026-08-20 13:46:44 +0530

    chore(deps): Bump deps
    Signed-off-by: Anurag Rajawat <anurag@stepsecurity.io>

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,11 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': ['ts-jest', { tsconfig: { allowJs: true } }]
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@octokit|universal-user-agent|before-after-hook|node-fetch)/)'
+  ],
   verbose: true
 }
